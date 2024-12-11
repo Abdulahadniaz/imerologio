05df689c259d365e13575c3fb84ef8182714edec
Abdul Ahad <abdulahad1028@gmail.com>
2024-12-12 00:36:13 +0500

    code commented

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/events", async (req, res) => {
-  try {
-    const auth = await authorize();
-    const events = await listEvents(auth);
-    res.json(events);
-  } catch (error) {
-    console.error("Error fetching events:", error);
-    res.status(500).json({ error: "Failed to fetch events" });
-  }
-});
+// app.get("/events", async (req, res) => {
+//   try {
+//     const auth = await authorize();
+//     const events = await listEvents(auth);
+//     res.json(events);
+//   } catch (error) {
+//     console.error("Error fetching events:", error);
+//     res.status(500).json({ error: "Failed to fetch events" });
+//   }
+// });
 
 // app.post("/create-event", async (req, res) => {
 //   const event = {
